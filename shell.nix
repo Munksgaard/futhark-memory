e3aae681c62da563ec208656db817a143230d05b
Philip Munksgaard <philip@munksgaard.me>
2022-02-09 12:09:43 +0100

    Simplify shell.nix

--- shell.nix
+++ shell.nix
@@ -2,12 +2,7 @@ let
   sources = import ./futhark/nix/sources.nix;
   pkgs = import sources.nixpkgs { };
   futhark_shell = import ./futhark/shell.nix;
-
-in let
-  pythonEnv =
-    (pkgs.python3.withPackages (ps: [ ps.numpy ps.matplotlib ])).override
-    (args: { ignoreCollisions = true; });
 in futhark_shell.overrideAttrs (oldAttrs: rec {
-  buildInputs = oldAttrs.buildInputs
-    ++ [ pythonEnv pkgs.moreutils pkgs.jq ];
+  buildInputs =  oldAttrs.buildInputs ++
+   [ pkgs.python3Packages.matplotlib pkgs.moreutils pkgs.jq ];
 })
